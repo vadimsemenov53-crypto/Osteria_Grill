6fde7464f2e861b84386df94de716992e8b2d473
Vadim Semenov <vadimsemenov53@gmail.com>
2026-09-13 11:18:39 +0300

    add new app content for dinamic content, add model ContentRest..About, add admin

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from content.models import ContentRestaurantAbout
 
-# Register your models here.
+@admin.register(ContentRestaurantAbout)
+class AdminContentRestaurantAbout(admin.ModelAdmin):
+    """Админка управления - модель ContentRestaurantAbout.
+    (управление контентом страницы 'о ресторане')"""
+
+    fieldsets = (
+        (
+            "История ресторана",
+            {
+                "fields": (
+                    "image_history",
+                    "history_title",
+                    "history_p1",
+                    "history_p2",
+                )
+            },
+        ),
+        (
+            "История шефа",
+            {
+                "fields": (
+                    "chef_image",
+                    "chef_title",
+                    "chef_p1",
+                    "chef_p2",
+                )
+            },
+        ),
+    )
+
+    # def has_add_permission(self, request):
+    #     """Запрещаем создавать больше одной записи."""
+    #     return not ContentRestaurantAbout.objects.exists()
+    #
+    # def has_delete_permission(self, request, obj=None):
+    #     """Запрещаем удалять единственную запись контента."""
+    #     return False
--- content/models.py
+++ content/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+class ContentRestaurantAbout(models.Model):
+    """ Модель для представления страницы 'О ресторане' (изменения - управления контентом). """
+
+    # История ресторана
+    image_history = models.ImageField(
+        upload_to="content/history_rest",
+        verbose_name="Фото для истории",
+        help_text="Загрузите для истории ресторана",
+    )
+
+    history_title = models.CharField(
+        max_length=500,
+        verbose_name="Заголовок истории ресторана.",
+        help_text="Введите заголовок для истории ресторана."
+    )
+    history_p1 = models.TextField(
+        verbose_name="Первый пункт",
+        help_text="Введите историю ресторана",
+    )
+
+    history_p2 = models.TextField(
+        verbose_name="Второй пункт",
+        help_text="Введите историю ресторана. Для нового абзаца оставьте пустую строку.",
+        blank=True,
+    )
+
+    # История шефа
+    chef_image = models.ImageField(
+        upload_to="content/history_chef",
+        verbose_name="Фото для Шеф-повара",
+        help_text="Загрузите для истории Шеф-повара",
+    )
+    chef_title = models.CharField(
+        max_length=500,
+        verbose_name="Заголовок истории Шеф-повара",
+        help_text="Укажите заголовок для истории о Шеф-поваре",
+    )
+
+    chef_p1 = models.TextField(
+        verbose_name="История шефа (видимое)",
+        help_text="Введите историю шефа",
+    )
+
+    chef_p2 = models.TextField(
+        verbose_name="История шефа (скрытое)",
+        help_text="Введите историю шефа. Для нового абзаца оставьте пустую строку.",
+    )
+
+    class Meta:
+        verbose_name = "Страница 'О ресторане'"
+
+# <p class="lead text-body">
+#     {{ content.chef_story|linebreaks }}
+# </p>
