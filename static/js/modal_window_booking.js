71b5fc9db53b1f4be153d58d12b571feae07e25b
Vadim Semenov <vadimsemenov53@gmail.com>
2026-09-15 21:52:38 +0300

    add display element table for window_booking.html, rebuild modal_wind.._booking.js -> textContent

--- static/js/modal_window_booking.js
+++ static/js/modal_window_booking.js
@@ -11,6 +11,15 @@ document.addEventListener("DOMContentLoaded", function () {
     // ищем modal по id  (class="modal fade" id="bookingModal"..)
     const modalElement = document.getElementById("bookingModal");
 
+    // ищем элемент по id ( <h5 id="bookingTableNumber"> Стол </h5> )
+    const bookingTableNumber = document.getElementById("bookingTableNumber");
+
+    // ищем элемент по id ( <p id="bookingTableCapacity" class="text-body-secondary"> </p> )
+    const bookingTableCapacity = document.getElementById("bookingTableCapacity");
+
+    // ищем элемент по id ( <strong id="bookingTableCategory"></strong> )
+    const bookingTableCategory = document.getElementById("bookingTableCategory");
+
     if (modalElement) { //проверка истинности существования окна
 
         //создание управляемого modal-окна
@@ -23,6 +32,23 @@ document.addEventListener("DOMContentLoaded", function () {
             // отслеживание нажатия по столу (<button class="restaurant-table> №{{ table.number }} </button>)
             table.addEventListener("click", function () {
                 // если нажал покажи modal window (модальное окно)
+
+                // получаем данные с стола по которому нажали
+                const tableNumber = table.dataset.tableNumber;
+                const tableCapacity = table.dataset.tableCapacity;
+                const tableCategory = table.dataset.tableCategory;
+
+                // записываем в найденные элементы
+                bookingTableNumber.textContent = `Стол №${tableNumber}`;
+                bookingTableCapacity.textContent = `Вместимость: ${tableCapacity} мест`;
+
+                // очищаем перед проверкой (исключение отображение статуса на обычных столах)
+                bookingTableCategory.textContent = "";
+
+                if (tableCategory == "VIP") {
+                            bookingTableCategory.textContent = "Премиум";
+                    }
+
                 modal.show();
             });
 
